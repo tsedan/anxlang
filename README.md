3c4fc5dfa5c3801a1a2d4b800a3809930054700e
Tomer Sedan <tsedan@pm.me>
2023-01-28 21:54:41 -0500

    basic tokenizer!

--- README.md
+++ README.md
@@ -6,23 +6,25 @@ Anx is a highly experimental systems programming language written in C++.
 
 The purpose of Anx is to simplify the most common use cases for C, while also including a select few of the quality-of-life features that modern languages provide.
 
+We're starting simple, and expanding only once a first working version is created.
+
 Here are a list of all of the datatypes available in Anx:
 ```
-i8, i16, i32, i64, i128
-u8, u16, u32, u64, u128
-f32, f64
-bool
+i32
 ```
 
-Here's an example of a fibonacci function in Anx:
+Here's a basic sample of the language syntax:
 
 ```
-fn fib(n: i32) i32 {
-    if (n < 2) ret n;
-    ret fib(n - 1) + fib(n - 2);
+fn recurseAdd(n: i32, s: i32) i32 {
+    if (n == 0) ret s;
+    var x: i32 = s * 2 + 1;
+
+    ret recurseAdd(n - 1, x);
 }
 ```
 
+Everything below this point is just a draft and should not be taken seriously.
 Here's Anx reading user input and printing it back out:
 
 ```
@@ -52,7 +54,7 @@ fn main() void { # return type required, even if void
 Errors and safety are a first-class concern in Anx. Here's an example of error handing:
 
 ```
-fn open_file(path: *u8) !bool {
+fn openFile(path: *u8) !bool {
     # the ! indicates that this function can return an error
 
     # try to open the file. if an error occured, return it.
@@ -63,7 +65,7 @@ fn open_file(path: *u8) !bool {
 
 fn main() void {
     # try to open a file.
-    open_file("doesnt_exist.txt") catch |err| {
+    openFile("doesnt_exist.txt") catch |err| {
         # this block is executed if an error occured
         @out(f"Error: %s{err}\n");
     };
--- anx.cpp
+++ anx.cpp
@@ -13,27 +13,178 @@
 
 enum Token
 {
-    tok_eof = -1,
-    tok_eol = -2,
+    // general
+    tok_eof, // end of file
+    tok_eos, // end of statement
 
-    tok_scopes = -3,
-    tok_scopee = -4,
+    // grouping
+    tok_comma,  // ,
+    tok_curlys, // curly start
+    tok_curlye, // curly end
+    tok_parens, // paren start
+    tok_parene, // paren end
 
-    tok_fn = -5,
-    tok_ret = -6,
+    // functions
+    tok_fn,  // function
+    tok_ret, // return
 
-    tok_var = -7,
-    tok_let = -8,
+    // variables
+    tok_var,    // variable
+    tok_type,   // :
+    tok_assign, // =
 
-    tok_type = -9,
+    // comparison
+    tok_equal, // ==
 
-    tok_if = -10,
-    tok_else = -11,
-    tok_while = -12,
+    // operators
+    tok_mul, // *
+    tok_div, // /
+    tok_add, // +
+    tok_sub, // -
 
-    tok_identifier = -13,
-    tok_number = -14,
+    // control
+    tok_if, // if
+
+    // identifiers
+    tok_identifier, // identifier
+    tok_integer,    // literal integer
 };
 
-static std::string IdentifierStr;
-static double NumVal;
+static std::string idstr;
+static int32_t i32val;
+
+// Get the next token from stdin
+static Token gettok()
+{
+    static int lch = ' ';
+
+    // skip whitespace
+    while (isspace(lch))
+        lch = getchar();
+
+    if (isalpha(lch))
+    {
+        idstr = lch;
+        while (isalnum(lch = getchar()))
+            idstr += lch;
+
+        if (idstr == "fn")
+            return tok_fn;
+        if (idstr == "ret")
+            return tok_ret;
+        if (idstr == "var")
+            return tok_var;
+        if (idstr == "if")
+            return tok_if;
+
+        return tok_identifier;
+    }
+
+    if (isdigit(lch))
+    {
+        std::string nstr;
+        do
+        {
+            nstr += lch;
+            lch = getchar();
+        } while (isdigit(lch));
+
+        i32val = atoi(nstr.c_str());
+        return tok_integer;
+    }
+
+    if (lch == '#')
+    {
+        do
+            lch = getchar();
+        while (lch != EOF && lch != '\n' && lch != '\r');
+
+        if (lch != EOF)
+            return gettok();
+    }
+
+    if (lch == ';')
+    {
+        lch = getchar();
+        return tok_eos;
+    }
+
+    if (lch == ',')
+    {
+        lch = getchar();
+        return tok_comma;
+    }
+
+    if (lch == '{')
+    {
+        lch = getchar();
+        return tok_curlys;
+    }
+
+    if (lch == '}')
+    {
+        lch = getchar();
+        return tok_curlye;
+    }
+
+    if (lch == '(')
+    {
+        lch = getchar();
+        return tok_parens;
+    }
+
+    if (lch == ')')
+    {
+        lch = getchar();
+        return tok_parene;
+    }
+
+    if (lch == ':')
+    {
+        lch = getchar();
+        return tok_type;
+    }
+
+    if (lch == '*')
+    {
+        lch = getchar();
+        return tok_mul;
+    }
+
+    if (lch == '/')
+    {
+        lch = getchar();
+        return tok_div;
+    }
+
+    if (lch == '+')
+    {
+        lch = getchar();
+        return tok_add;
+    }
+
+    if (lch == '-')
+    {
+        lch = getchar();
+        return tok_sub;
+    }
+
+    if (lch == '=')
+    {
+        lch = getchar();
+
+        if (lch == '=')
+        {
+            lch = getchar();
+            return tok_equal;
+        }
+
+        return tok_assign;
+    }
+
+    if (lch == EOF)
+        return tok_eof;
+
+    printf("Unknown token '%c'", lch);
+    exit(1);
+}
